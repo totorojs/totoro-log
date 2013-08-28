05e907979f39749fe6aa664d1f9d01d90b5704c6
kangpangpang <kangpangpang@gmail.com>
2013-08-28 17:10:43 +0800

    U: refactor

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 'use strict';
 
 module.exports = require('./lib/logger.js')
+
+module.exports.getConsoleTransport = require('./lib/console-transport').get
+module.exports.getFileTransport = require('./lib/file-transport').get
+module.exports.getMongoTransport = require('./lib/mongo-transport').get
+module.exports.Transport = require('./lib/transport')
